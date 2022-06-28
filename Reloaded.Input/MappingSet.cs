6722227542d255ee0e179df1915747291e001fa7
Sewer 56 <seweratwork@gmail.com>
2022-06-28 20:45:17 +0100

    Changed: Newtonsoft Begone!

--- Reloaded.Input/MappingSet.cs
+++ Reloaded.Input/MappingSet.cs
@@ -1,31 +1,31 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
+using System.Text.Json;
 using Reloaded.Input.Structs;
 
 namespace Reloaded.Input;
 
 public class MappingSet
 {
-    public Dictionary<int, Mapping> Mappings { get; private set; } = new();
+    public Dictionary<int, Mapping> Mappings { get; set; } = new();
 
     // Serialization Methods
     public void SaveTo(string filePath)
     {
-        File.WriteAllText(filePath, JsonConvert.SerializeObject(this, _serializerSettings));
+        File.WriteAllText(filePath, JsonSerializer.Serialize(this, _serializerSettings));
     }
 
     public static MappingSet ReadOrCreateFrom(string filePath)
     {
         var result = File.Exists(filePath)
-            ? JsonConvert.DeserializeObject<MappingSet>(File.ReadAllText(filePath), _serializerSettings)
+            ? JsonSerializer.Deserialize<MappingSet>(File.ReadAllText(filePath), _serializerSettings)
             : new MappingSet();
 
         return result;
     }
 
-    private static JsonSerializerSettings _serializerSettings = new()
+    private static JsonSerializerOptions _serializerSettings = new()
     {
-        Formatting = Formatting.Indented
+        WriteIndented = true
     };
 }
