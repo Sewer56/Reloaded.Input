aff89de1aadbf299e37a2a25743321bc97b6701c
Sewer 56 <seweratwork@gmail.com>
2022-07-02 17:09:59 +0100

    Added: Support for hiding configuration dropdown when there's only 1 config.

---
+++ Reloaded.Input.Configurator/WPF/Converter/ConfigurationsToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Reloaded.Input.Configurator.Model;
+
+namespace Reloaded.Input.Configurator.WPF.Converter;
+
+internal class ConfigurationsToVisibilityConverter : IValueConverter
+{
+    public static ConfigurationsToVisibilityConverter Instance = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ObservableCollection<Configuration> { Count: > 1 })
+            return Visibility.Visible;
+
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
